b9cdf91c1352ec29938af52080f43908a1865e2f
madhava <galarzaguillermo.ggm@gmail.com>
2025-10-23 08:20:03 -0300

    fix(actions): set up
    Signed-off-by: madhava <galarzaguillermo.ggm@gmail.com>

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 import { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, NODE_ENV } from "../config";
-console.log({ DB_NAME, DB_USER, DB_PASSWORD, DB_HOST });
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
