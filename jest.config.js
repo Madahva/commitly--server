a33086d594fb00c40055f0f83ce7489ad2c827d7
madhava <galarzaguillermo.ggm@gmail.com>
2026-01-25 04:30:41 -0300

    feat(env): add env
    Signed-off-by: madhava <galarzaguillermo.ggm@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  collectCoverageFrom: ["src/**/*.ts", "!src/server.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/scripts/**",
+    "!src/middlewares/auth0.middleware.ts",
+    "!src/database/connection.ts",
+    "!src/server.ts",
+  ],
   setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
 };
